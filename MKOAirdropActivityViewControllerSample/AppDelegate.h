6204eb969f5ef250d1d7614ebb51a996bc85e20e
Mathias Koehnke <mathias.koehnke@web.de>
2015-06-20 07:54:13 +0200

    Initial Commit

---
+++ MKOAirdropActivityViewControllerSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MKOAirdropActivityViewControllerSample
+//
+//  Created by Mathias Köhnke on 20/06/15.
+//  Copyright (c) 2015 Mathias Koehnke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MKOAirdropActivityViewControllerSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MKOAirdropActivityViewControllerSample
+//
+//  Created by Mathias Köhnke on 20/06/15.
+//  Copyright (c) 2015 Mathias Koehnke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
