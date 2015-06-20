a6b656b4fdd918decdf48d0101dd2caf5e6d295a
Mathias Koehnke <mathias.koehnke@web.de>
2015-06-20 08:36:55 +0200

    License + Added iPad Support to Sample

--- Example/MKOAirdropActivityViewControllerSample/ViewController.m
+++ Example/MKOAirdropActivityViewControllerSample/ViewController.m
@@ -1,20 +1,45 @@
 //
-//  ViewController.m
-//  MKOAirdropActivityViewControllerSample
+// ViewController.m
 //
-//  Created by Mathias Köhnke on 20/06/15.
-//  Copyright (c) 2015 Mathias Koehnke. All rights reserved.
+// Copyright (c) 2015 Mathias Koehnke (http://www.mathiaskoehnke.com)
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "ViewController.h"
 #import "MKOAirdropActivityViewController.h"
 
+@interface ViewController ()
+@property (nonatomic, strong) UIPopoverController *popover;
+@end
+
 @implementation ViewController
 
-- (IBAction)showAirDrop {
+- (IBAction)showAirDrop:(UIButton *)button {
     UIImage *image = [UIImage imageNamed:@"octocat"];
     MKOAirdropActivityViewController *vc = [[MKOAirdropActivityViewController alloc] initWithActivityItems:@[image] applicationActivities:nil];
-    [self presentViewController:vc animated:YES completion:nil];
+    
+    if ([UIDevice currentDevice].userInterfaceIdiom == UIUserInterfaceIdiomPad) {
+        self.popover = [[UIPopoverController alloc] initWithContentViewController:vc];
+        [self.popover presentPopoverFromRect:button.frame inView:self.view permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    } else {
+        [self presentViewController:vc animated:YES completion:nil];
+    }
 }
 
 @end
--- MKOAirdropViewController/MKOAirdropActivityViewController.m
+++ MKOAirdropViewController/MKOAirdropActivityViewController.m
@@ -1,10 +1,25 @@
 //
-//  MKOAirdropViewController.m
-//  MKOAirdropViewControllerSample
+//  MKOAirdropActivityViewController.m
 //
-//  Created by Mathias Köhnke on 19/06/15.
-//  Copyright (c) 2015 Mathias Koehnke. All rights reserved.
+// Copyright (c) 2015 Mathias Koehnke (http://www.mathiaskoehnke.com)
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "MKOAirdropActivityViewController.h"
 
@@ -87,7 +102,7 @@ static NSUInteger MKONumberOfActivitySections = 3;
 
 
 - (void)setExcludedActivityTypes:(NSArray *)excludedActivityTypes {
-    // 
+    // In order to have a consistent number of sections, we ignore excluded activity types here.
 }
 
 @end
